e09f09e5539c2257d022f63200e0ee18ae751827
Thomas DELMARE <delmarethomas1@gmail.com>
2022-09-26 17:00:29 +0200

    First commit

---
+++ scripts/reservation.js
@@ -0,0 +1,1 @@
+ListeArrets = ["Arret St-Jean", "Arret Les Moulins", "Arret Place du Cap", "Arret St-Roch", "Arret Lycée Louis-Jean Le Valois", "Arret Les Vallets", "Arret Hello World", "Arret Place de sa Majesté Delmare", "Arret Les Genets"];

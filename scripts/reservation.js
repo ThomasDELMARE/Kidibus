974a6a919a8845e6c0720b9276defb5cb2c58f0b
Thomas DELMARE <71331024+ThomasDELMARE@users.noreply.github.com>
2022-09-26 18:48:26 +0200

    Elias avancement

--- scripts/reservation.js
+++ scripts/reservation.js
@@ -1,3 +1,2 @@
 ListeArrets = ["Arret St-Jean", "Arret Les Moulins", "Arret Place du Cap", "Arret St-Roch", "Arret Lycée Louis-Jean Le Valois", "Arret Les Vallets", "Arret Hello World", "Arret Place de sa Majesté Delmare", "Arret Les Genets"];
 
-document.querySelector('submit').disabled = true;
